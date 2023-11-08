40269e67733687d184f52a7c6fd9f98811eadbc7
Helena McMonagle <43573863+hmcmonagle@users.noreply.github.com>
2023-11-08 10:55:40 -0800

    uploading resources as links
    Some resources that were helpful for me as I set up copilot for us in R studio. I threw in an excellent resources on setting up Github too for folks who might be looking for instructions on that, too.

---
+++ Resources for setting up Copilot in R Studio.R
@@ -0,0 +1,21 @@
+# Graduate Student Symposium talk on Copilot for R
+# Helena McMonagle
+# November 2023
+
+# Copilot is a tool that uses AI to help you write code. It's now available 
+# for use with the R coding language and can be used within R Studio.
+
+# The links below are resources that may be references during the GSS presention
+# on this topic and were helpful for setting up Copilot for R. 
+
+# Instructions for getting a free Github Pro account as a student: 
+# https://techcommunity.microsoft.com/t5/educator-developer-blog/step-by-step-setting-up-github-student-and-github-copilot-as-an/ba-p/3736279#:~:text=Setting%20Up%20Copilot&text=You%20should%20now%20be%20able,Github%20Student%20Developer%20Pack%20benefits
+
+# Instructions for setting up Copilot within R Studio: 
+# https://docs.posit.co/ide/user/ide/guide/tools/copilot.html
+
+# Not necessary for Copilot, but if you're new to Github and looking for instructions
+# for how to set up version control so you can backup your work with Github 
+# (by the end, you'll know exactly how to commit, push and pull!), I highly
+# recommend this bookdown resource called "Happy Git and Github for the useR"
+# https://happygitwithr.com
